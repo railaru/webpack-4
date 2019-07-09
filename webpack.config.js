8d02d332f31b13b6ee4bc5e5a36aae0f9d9bdf85
Rutenis <r.raila@keepthinking.it>
2019-07-09 12:48:23 +0100

    ouput css to main.css, not inside js

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
+        use: [miniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
       },
       {
         test: /\.html$/,
